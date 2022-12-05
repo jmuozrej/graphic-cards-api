5f1abbc81d3e843d1061479267b33390b105ebca
jmuozrej <juanjose.munoz-reja@optivamedia.com>
2022-12-05 13:01:32 +0100

    remove node modules and add cors dependency to avoit it

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
-const app = express()               
+const app = express()
+const cors = require('cors')              
 
 const port = process.env.PORT || 8080
+app.use(cors())
 
 const graphics = [
     {
