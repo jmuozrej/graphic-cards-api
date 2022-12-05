db7822002597683c461694debaff002c10a045b2
jmuozrej <juanjose.munoz-reja@optivamedia.com>
2022-12-05 14:04:43 +0100

    create readme and add img

---
+++ README.md
@@ -0,0 +1,9 @@
+First, the necessary dependencies are installed
+
+### `npm install`
+
+If there are any errors, the development environment is restarted
+
+Finally, the application is deployed
+
+### `npm start`
--- index.js
+++ index.js
@@ -11,84 +11,96 @@ const graphics = [
         name: 'NVIDIA',
         price: 100,
         model: '4090',
-        ram: 'GDDR6'
+        ram: 'GDDR6',
+        img: 'https://i.blogs.es/4cfcd3/geforce_gtx_1080_3/1366_2000.jpg'
     },
     {
         id: 1,
         name: 'NVIDIA',
         price: 200,
         model: '4080',
-        ram: 'GDDR5'
+        ram: 'GDDR5',
+        img: 'https://i.blogs.es/4cfcd3/geforce_gtx_1080_3/1366_2000.jpg'
     },
     {
         id: 2,
         name: 'NVIDIA',
         price: 300,
         model: '1080',
-        ram: 'GDDR4'
+        ram: 'GDDR4',
+        img: 'https://i.blogs.es/4cfcd3/geforce_gtx_1080_3/1366_2000.jpg'
     },
     {
         id: 3,
         name: 'NVIDIA',
         price: 400,
         model: '1070',
-        ram: 'GDDR4'
+        ram: 'GDDR4',
+        img: 'https://i.blogs.es/4cfcd3/geforce_gtx_1080_3/1366_2000.jpg'
     },
     {
         id: 4,
         name: 'NVIDIA',
         price: 500,
         model: '4080',
-        ram: 'GDDR6'
+        ram: 'GDDR6',
+        img: 'https://i.blogs.es/4cfcd3/geforce_gtx_1080_3/1366_2000.jpg'
     },
     {
         id: 5,
         name: 'NVIDIA',
         price: 600,
         model: '2080',
-        ram: 'GDDR5'
+        ram: 'GDDR5',
+        img: 'https://i.blogs.es/4cfcd3/geforce_gtx_1080_3/1366_2000.jpg'
     },
     {
         id: 6,
         name: 'AMD',
         price: 100,
         model: '6650',
-        ram: 'GDDR4'
+        ram: 'GDDR4',
+        img: 'https://i.blogs.es/66e808/amd-6990/450_1000.jpg'
     },
     {
         id: 7,
         name: 'AMD',
         price: 200,
         model: '6750',
-        ram: 'GDDR5'
+        ram: 'GDDR5',
+        img: 'https://i.blogs.es/66e808/amd-6990/450_1000.jpg'
     },
     {
         id: 8,
         name: 'AMD',
         price: 300,
         model: '6600',
-        ram: 'GDDR5'
+        ram: 'GDDR5',
+        img: 'https://i.blogs.es/66e808/amd-6990/450_1000.jpg'
     },
     {
         id: 9,
         name: 'AMD',
         price: 400,
         model: '6700',
-        ram: 'GDDR6'
+        ram: 'GDDR6',
+        img: 'https://i.blogs.es/66e808/amd-6990/450_1000.jpg'
     },
     {
         id: 10,
         name: 'AMD',
         price: 500,
         model: '6800',
-        ram: 'GDDR5'
+        ram: 'GDDR5',
+        img: 'https://i.blogs.es/66e808/amd-6990/450_1000.jpg'
     },
     {
         id: 11,
         name: 'AMD',
         price: 600,
         model: '6600',
-        ram: 'GDDR4'
+        ram: 'GDDR4',
+        img: 'https://i.blogs.es/66e808/amd-6990/450_1000.jpg'
     }
 ]
 
